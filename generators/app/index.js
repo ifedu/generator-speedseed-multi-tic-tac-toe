1cea63a3b5b43dab27ea0eb68a023bd2a7383ab7
Eduardo Pérez-Bermejo <ifedu@outlook.com>
2016-06-26 12:27:34 +0200

    v.0.2.0

--- generators/app/index.js
+++ generators/app/index.js
@@ -13,28 +13,57 @@ module.exports = class Yo extends speedseed.Yo {
         super.paths()
     }
 
+    prompting() {
+        super.promptingOptions({
+            framework: {
+                AngularJS: true,
+                Angular2: true,
+                jQuery: true,
+                Polymer: true,
+                React: true,
+                VanillaJS: true
+            },
+
+            compiler: {
+                BabelJS: true,
+                Coffeescript: false,
+                TypeScript: true
+            },
+
+            css: {
+                SaSS: true,
+                ScSS: true,
+                Less: true,
+                Stylus: true,
+            },
+
+            test: {
+                Mocha: true,
+                Jasmine: true
+            }
+        })
+    }
+
     write() {
         const $ = super.configGet()
 
         const route = `seed/template/${$.template}`
+
+        // FRAMEWORK
+        super.create(`${route}/all/compiler/${$.compiler}/app`, './app', true)
+        super.create(`${route}/${$.framework}/compiler/all/app`, './app', true)
+        super.create(`${route}/${$.framework}/compiler/${$.compiler}/app`, './app', true)
+        // CSS
+        super.create(`${route}/all/css/${$.css}/app`, './app', true)
+        super.create(`${route}/${$.framework}/css/${$.css}/app`, './app', true)
+        // SERVER
+        super.create(`${route}/all/server/**/*`, './server', true)
 
         // COMPILER
         if ($.compiler === 'typescript') {
             super.create(`${route}/all/compiler/${$.compiler}/tsconfig.json`, './tsconfig.json')
             super.create(`${route}/${$.framework}/compiler/${$.compiler}/typings`, './typings')
             super.create(`${route}/${$.framework}/compiler/${$.compiler}/typings.json`, './typings.json')
-        }
-
-        if ($.template !== 'no') {
-            // FRAMEWORK
-            super.create(`${route}/all/compiler/${$.compiler}/app`, './app', true)
-            super.create(`${route}/${$.framework}/compiler/all/app`, './app', true)
-            super.create(`${route}/${$.framework}/compiler/${$.compiler}/app`, './app', true)
-            // CSS
-            super.create(`${route}/all/css/${$.css}/app`, './app', true)
-            super.create(`${route}/${$.framework}/css/${$.css}/app`, './app', true)
-            // SERVER
-            super.create(`${route}/all/server/**/*`, './server', true)
         }
 
         // TEST
